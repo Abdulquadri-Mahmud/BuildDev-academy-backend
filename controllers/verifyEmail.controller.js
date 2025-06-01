75aad9446374eb3c2a64145d6ef170fa70886cd4
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2025-06-01 18:17:17 +0100

    verify email frontend route added to the student controller

--- controllers/verifyEmail.controller.js
+++ controllers/verifyEmail.controller.js
@@ -20,20 +20,19 @@ export const verifyEmail = async (req, res) => {
       });
     }
 
-    if (user.isVerified) {
-      return res.status(200).json({
-        success: true,
-        message: "Email already verified. You can proceed to login.",
-      });
-    }
+    // if (user.isVerified) {
+    //   return res.status(200).json({
+    //     success: true,
+    //     message: "Email already verified. You can proceed to login.",
+    //   });
+    // }
 
-    
     user.isVerified = true;
-    // user.verificationToken = undefined;
+    user.verificationToken = undefined;
     
     await user.save();
     
-    return res.status(200).json({
+    res.status(200).json({
       success: true,
       message: "Email successfully verified. You can now log in.",
     });
