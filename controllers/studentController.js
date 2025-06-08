dd5397b4ccba22f900eb7dfa6d70fc5a6dcbd345
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2025-06-08 19:42:32 +0100

    changes made

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -56,7 +56,7 @@ export const registerStudent = async (req, res) => {
     await student.save();
 
     // Create verify URL
-    const verifyUrl = `https://dev-dynamos.onrender.com/verify-email?token=${token}`;
+    const verifyUrl = `https://samka-tech.vercel.app/verify-email?token=${token}`;
 
     // Send welcome + verification email
     await sendWelcomeMailMessage(email, firstName, lastName, verifyUrl);
